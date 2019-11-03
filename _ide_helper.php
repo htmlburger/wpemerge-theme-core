6fe77f912173daafdd96590169d0aeab8218a25d
Atanas Angelov <hi@atanas.dev>
2019-11-03 12:22:03 +0200

    Add an IDE helper file

---
+++ _ide_helper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * A helper file for aliased methods.
+ */
+namespace  {
+	exit( 'This file should not be included, only analyzed by your IDE.' );
+}
+
+namespace WPEmerge\Application {
+	class Application {
+		/**
+		 * Get the theme utilities instance.
+		 *
+		 * @return \WPEmergeTheme\Theme\Theme
+		 */
+		public static function theme() {}
+	}
+}
